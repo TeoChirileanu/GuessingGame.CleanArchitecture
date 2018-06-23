62ba64f9fe261892ffb95f00947bc61d03404bf9
Teodor Chirileanu <teodor@mobibam.com>
2018-06-24 01:46:43 +0200

    now featuring sms game!

--- GuessingGame.Console/ConsoleApplication.cs
+++ GuessingGame.Console/ConsoleApplication.cs
@@ -4,12 +4,18 @@ using GuessingGame.Properties;
 namespace GuessingGame.ConsoleApplication {
     public class ConsoleApplication {
         private static void Main() {
-            PlayGame();
+
+            try {
+                PlayGame();
+            }
+            catch (Exception e) {
+                Console.WriteLine(Properties.Resources.GameOverMessage + e.Message);
+            }
         }
 
         private static void PlayGame() {
             Console.WriteLine(Resources.WelcomeMessage);
-            var controller = new GameController(new Game(new ConsoleMessenger()), new KeyboardGetter());
+            var controller = new GameController(new Game(new SmsMessenger()), new KeyboardGetter());
             do {
                 int number = controller.GetNumber();
                 controller.CheckNumber(number);
---
+++ GuessingGame.Console/SmsMessenger.cs
@@ -0,0 +1,17 @@
+﻿using GuessingGame.ConsoleApplication.Properties;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+namespace GuessingGame.ConsoleApplication {
+    public class SmsMessenger : IMessenger {
+        public void Deliver(string message) {
+            TwilioClient.Init(Resources.TwilioAccountSid, Resources.TwilioAuthToken);
+
+            MessageResource.Create(
+                new PhoneNumber("+436764178797"),
+                from: new PhoneNumber(Resources.TwilioPhoneNumber),
+                body: message);
+        }
+    }
+}
