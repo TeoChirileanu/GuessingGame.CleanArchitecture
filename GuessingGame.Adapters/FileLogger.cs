1e8fd4e952149e0a32c3468791236aec28674781
Teodor Chirileanu <teodor@mobibam.com>
2018-06-27 22:11:13 +0200

    implemented filelogger

--- GuessingGame.Adapters/FileLogger.cs
+++ GuessingGame.Adapters/FileLogger.cs
@@ -1,18 +1,29 @@
 ﻿using System;
+using System.IO;
 using GuessingGame.BusinessRules;
+using GuessingGame.Shared.Properties;
 
 namespace GuessingGame.Adapters {
     public class FileLogger : ILogger {
+        private readonly string _pathToFile = Path.Combine(Resources.GameDirectory, "number.log");
+        private string FullPathToFile => Environment.ExpandEnvironmentVariables(_pathToFile);
+
         public void Log(string message) {
-            throw new NotImplementedException();
+            using (var stream = new StreamWriter(FullPathToFile, true)) {
+                stream.WriteLine(message);
+            }
         }
 
         public string GetLog() {
-            throw new NotImplementedException();
+            using (var stream = new StreamReader(FullPathToFile)) {
+                return stream.ReadToEnd();
+            }
         }
 
         public void ClearLog() {
-            throw new NotImplementedException();
+            using (var stream = new StreamWriter(FullPathToFile)) {
+                stream.Write(string.Empty);
+            }
         }
     }
 }
