8ea06955cd72d44368a4c5f681b456e5a1fa2c3f
Teodor Chirileanu <teodor@mobibam.com>
2018-07-09 13:26:28 +0200

    added mobile still stuff to do

---
+++ MobileApplication/MobileApplication/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace MobileApplication {
+    public partial class MainPage : ContentPage {
+        public MainPage() {
+            InitializeComponent();
+        }
+
+        private void IncrementNumber(object sender, EventArgs e) {
+            if (!(sender is Entry entry)) {
+                BindingContext = "Could not cast to Entry";
+                return;
+            }
+            bool parsedSuccessfully = int.TryParse(entry.Text, out int number);
+            if (!parsedSuccessfully) {
+                BindingContext = "Could not parse to Int";
+                return;
+            }
+            number++;
+            BindingContext = number.ToString();
+        }
+    }
+}
